89f92c0978c66f45c90d2eefca72cfbf3f850629
Villivateur <villivateur@gmail.com>
2018-07-22 10:28:48 +0800

    把苏瑞辅的神器修改下

--- src/index.php
+++ src/index.php
@@ -1,23 +1,11 @@
 <?php
-require 'Wechat.class.php';
+require 'wechat.class.php';
 
 $object = new WeChat("onMessage");
 $object->process();
 
 function onMessage(WeChat $object, $messageType, $content, $arg1, $arg2)
 {
-    switch ($messageType) {
-            case "subscribe":
-                   
-                break;
-            case "text":
-                if( ) {
-
-                }
-                break;
-            case "click":
-                if( ) {
-
-                }
-        }
+        if ($content=='苏瑞辅') $object->sendText("傻逼一个");
+        elseif( $content=="冯锦瑾") $object->sendText("天下无敌");
 }
